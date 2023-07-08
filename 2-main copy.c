7690b3ee79a3fe7aa9681dfb1c35a37fd7ddd252
oyeniran samuel sanmi <sossesam@gmail.com>
2023-07-08 17:32:26 +0100

    debugging

--- 2-main copy.c
+++ 2-main copy.c
@@ -10,9 +10,10 @@
  */
 int main(void)
 {
+	int x,y;
+ x =_printf("%u", -1024);
+ y = printf("%u", -1024);
 
- _printf("%b", -1024);
-
-	
+printf("myprintf = %d, printf = %d \n", x, y);
 	return (0);
 }
